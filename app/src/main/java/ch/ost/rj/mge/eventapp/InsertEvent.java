2ed549c63e763de5d800a17d00656ed73e6c9a6e
RamonFan69 <jerome.oehninger@ost.ch>
2022-10-30 15:11:31 +0100

    added DatePicker

--- app/src/main/java/ch/ost/rj/mge/eventapp/InsertEvent.java
+++ app/src/main/java/ch/ost/rj/mge/eventapp/InsertEvent.java
@@ -2,22 +2,33 @@ package ch.ost.rj.mge.eventapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class InsertEvent extends AppCompatActivity
 implements AdapterView.OnItemSelectedListener {
 
     String[] departments = { "Alle", "Informatik", "Elektrotechnik", "WING", "EEU"};
+    Calendar myCalendar  = Calendar.getInstance();
+    EditText text_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_event);
+
+        EditText text_title = findViewById(R.id.text_input_title);
 
         Spinner spin = (Spinner) findViewById(R.id.departments_spinner);
         spin.setOnItemSelectedListener(this);
@@ -25,15 +36,42 @@ implements AdapterView.OnItemSelectedListener {
         ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,departments);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(aa);
+
+        EditText text_location = findViewById(R.id.text_input_where);
+
+        EditText text_creator = findViewById(R.id.text_input_creator);
+
+        text_date = findViewById(R.id.text_input_date_picker);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONDAY, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabel();
+            }
+        };
+        text_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(InsertEvent.this, date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        Toast.makeText(getApplicationContext(),departments[position] , Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), departments[position], Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    private void updateLabel() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy", Locale.GERMAN);
+        text_date.setText(dateFormat.format(myCalendar.getTime()));
     }
 }
