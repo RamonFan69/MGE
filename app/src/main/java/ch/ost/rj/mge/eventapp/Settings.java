e037a00d3285f481bb6e2ecdd990b654d41222f3
Kevin.Pfister <kevin.pfiste@ost.ch>
2022-11-18 13:13:39 +0100

    Update Settings.java
    Navigation Bar am debugge

--- app/src/main/java/ch/ost/rj/mge/eventapp/Settings.java
+++ app/src/main/java/ch/ost/rj/mge/eventapp/Settings.java
@@ -3,19 +3,23 @@ package ch.ost.rj.mge.eventapp;
 
 
 
+import static ch.ost.rj.mge.eventapp.MainActivity.logStateChange;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
-    public DrawerLayout drawerLayout;
+    public DrawerLayout drawerLayout_settings;
     public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
@@ -25,17 +29,39 @@ public class Settings extends AppCompatActivity {
 
         // drawer layout instance to toggle the menu icon to open
         // drawer and back button to close drawer
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout_settings = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout_settings, R.string.nav_open, R.string.nav_close);
 
         // pass the Open and Close toggle for the drawer layout listener
         // to toggle the button
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        drawerLayout_settings.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public boolean onNavigationItemSelected(MenuItem Item) {
+        logStateChange("Methode erreicht");
+        switch (Item.getItemId()) {
+            case R.id.einstellungen:
+                Intent intent = new Intent(this, Settings.class);
+                this.startActivity(intent);
+                logStateChange("button Pressed");
+                return true;
+            case R.id.hinzugefuegte_events:
+                //Intent intent = new Intent(this,);
+        }
+        return false;
+    }
+
 
 
 
